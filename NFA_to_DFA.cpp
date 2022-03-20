b8d64e0ab418090571cb7b5f40074b39a16361a8
Kamruzzaman Asif <72350695+KamruzzamanAsif@users.noreply.github.com>
2022-03-20 22:30:46 +0600

    Add files via upload

--- NFA_to_DFA.cpp
+++ NFA_to_DFA.cpp
@@ -7,11 +7,43 @@ char nfa_states[N];
 char nfa_starting_state;
 int nfa_numberOfFinalStates;
 char nfa_final_states[N];
-//int nfa_transition_table[N][2];
-// we may use map
+string nfa_transitions[N][N]; // indexing(states) according to the order of nfa_states
+
+char dfa_starting_state;
+string dfa_states[N];
+string dfa_transitions[N][N];
+
+queue <string> done, to_be_done; 
+
+int findIndex(char c){
+    for(int i=0; i<nfa_numberOfStates; i++){
+        if(c==nfa_states[i])
+            return i;
+    }
+    return -1;
+}
+
+void generateDFA(){
+    dfa_starting_state = nfa_starting_state;
+    dfa_states[0] = dfa_starting_state;
+    done.push(dfa_states[0]);
+    int index = findIndex(dfa_starting_state);
+    to_be_done.push(nfa_transitions[index][0]);
+    to_be_done.push(nfa_transitions[index][1]);
+
+    int dfa_state_index = 1;
+    while(!to_be_done.empty()){
+        string temp = to_be_done.front();
+        to_be_done.pop();
+        done.push(temp);
+        dfa_states[dfa_state_index++] = temp;
+
+        
+    }
+}
 
 int main(void){
-
+    freopen("nfaToDfa.txt", "r", stdin);
     cout<<"Enter number of states of NFA: ";
     cin>>nfa_numberOfStates;
     cout<<endl;
@@ -40,6 +72,37 @@ int main(void){
     }
     cout<<endl;
 
+    // transitions of nfa
+    cout<<"Enter the transitions: "<<endl;
+    for(int i=0; i<nfa_numberOfStates; i++){
+        cout<<nfa_states[i]<<" using 0 goes to: ";
+        cout<<"(enter the states (enter 'x' to stop))";
+        char c;
+        string s;
+        cin>>c;
+        while(c!='x'){
+            s += c;
+            s += ' ';
+            cin>>c;
+        }
+        nfa_transitions[i][0] = s;
+        cout<<endl;
+
+        cout<<nfa_states[i]<<" using 1 goes to: ";
+        cout<<"(enter the states (enter 'x' to stop))";
+        char ch;
+        string t;
+        cin>>ch;
+        while(ch!='x'){
+            t += ch;
+            t += ' ';
+            cin>>ch;
+        }
+        nfa_transitions[i][1] = t;
+        cout<<endl;
+    }
+
+    generateDFA();
 
     return 0;
 }
